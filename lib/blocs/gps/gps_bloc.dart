b4deb683c14a72bf5f41f890f61dfcc5d4574d24
Boris Ricardo Miranda <brmiranda009@gmail.com>
2023-05-05 16:34:02 -0600

    agregando geolocator for android

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -8,6 +9,7 @@ part 'gps_state.dart';
 class GpsBloc extends Bloc<GpsEvent, GpsState> {
   GpsBloc() : super(const GpsState(isGpsEnable: false, isGpsPermissionGranted: false)) {
     on<GpsAndPermissionEvent>(onGPSAndPermission);
+    _init();
   }
 
   void onGPSAndPermission(GpsAndPermissionEvent event, Emitter<GpsState> emit) {
@@ -17,5 +19,23 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
         isGpsPermissionGranted: event.isGpsPermissionGranted,
       ),
     );
+  }
+
+  Future<void> _init() async {
+    await _checkGpsStatus();
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+    Geolocator.getServiceStatusStream().listen((event) {
+      print('Service Status $event');
+    });
+
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    return super.close();
   }
 }
